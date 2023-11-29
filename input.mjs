696fbf166e51978188ba6265fe35d01c1676d147
Sven <chanck.mds@gmail.com>
2023-11-29 17:04:43 +0100

    ex 5 is done

--- input.mjs
+++
@@ -1,4 +0,0 @@
-import isNumber from './isNumber.mjs';
-
-let input = 8;
-console.log(isNumber(input));
--- isNumber.mjs
+++
@@ -1,5 +0,0 @@
-function isNumber(n) {
-    return  typeof n === 'number' ? 'its a number' : 'its not a number';
-}
-
-export default isNumber;
