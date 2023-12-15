03467c5092dbea485ee85e01339e06a696ae7461
Sven <chanck.mds@gmail.com>
2023-12-15 16:45:35 +0100

    ex 17 is done

--- db.js
+++
@@ -1,4 +0,0 @@
-const pgp = require('pg-promise')();
-const db = pgp(process.env.DATABASE_URL || 'postgres://postgres:6938@localhost:5432/planetsdb');
-
-module.exports = db;
---
+++ planets.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { db } from "./db";
+
+
+const getAll = async (req: Request, res: Response) => {
+  const planets = await db.many(`SELECT * FROM planets;`);
+  res.json(planets);
+};
+
+const getOneById = async (req: Request, res: Response) => {
+  const planetId = parseInt(req.params.id, 10);
+  const planet = await db.oneOrNone(`SELECT * FROM planets WHERE id=$1`, planetId);
+  res.json(planet);
+};
+
+const create = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const newPlanet = { name };
+  await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+  res.status(201).json({ msg: "Planet created successfully" });
+};
+
+const updateById = async (req: Request, res: Response) => {
+  const {id} = req.params
+  const {name} = req.body
+  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name])
+  res.json({ msg: "Planet updated successfully" });
+};
+
+const deleteById = async (req: Request, res: Response) => {
+  const planetId = req.params.id;
+  await db.none(`DELETE FROM planets WHERE id=$1`, Number(planetId))
+  res.json({ msg: "Planet deleted successfully" });
+};
+
+const createImage = async (req: Request, res: Response) => {
+  const {id} = req.params
+  const fileName = req.file?.path
+  if (fileName) {
+    db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, fileName])
+    res.status(201).json({msg: "Planet image uploaded successfully."})
+  } else {
+    res.status(400).json({msg: "Planet image failed to uploaded."})
+  }
+}
+
+export { getAll, getOneById, create, updateById, deleteById, createImage };
